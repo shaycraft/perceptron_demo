e057781892cc12dbb3b0e6f3c44d3ead8cc53a00
Sam Haycraft <samuel.g.haycraft@xcelenergy.com>
2025-05-27 12:00:11 -0600

    first initial pass

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 
 
 class Perceptron(object):
@@ -13,15 +14,37 @@ class Perceptron(object):
     
     Attributes
     ----------
-    weights : 1d-array
+    _weights : 1d-array
         Weights after fitting.
-    errors : list
-        Number of misclassifications in each epoch.
+    _errors : list
+        Number of miss classifications in each epoch.
     """
 
     def __init__(self, eta=0.01, n_iter=10):
         self.eta = eta
         self.n_iter = n_iter
+        self._weights = None
+        self._errors = None
 
-    def fit(self, X, y):
-        self.weights = np.zeros(1+X.shape[1])
+    def fit(self, training_vectors, target_vectors):
+        self._weights = np.zeros(1 + training_vectors.shape[1])
+        self._errors = []
+
+        for i in range(self.n_iter):
+            errors = 0
+            for training, target in zip(training_vectors, target_vectors):
+                update = self.eta * (target - self.predict(training))
+                self._weights[1:] += update * training
+                self._weights[0] += update
+                errors += int(update != 0.0)
+            self._errors.append(errors)
+        return self
+
+    def net_input(self, x):
+        return np.dot(x, self._weights[1:]) + self._weights[0]
+
+    def predict(self, x):
+        return np.where(self.net_input(x) >= 0.0, 1, -1)
+
+df = pd.read_csv("https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data", header=None)
+print(df.tail())
